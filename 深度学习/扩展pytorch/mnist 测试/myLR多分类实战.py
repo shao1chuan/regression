501f6b6a740b78d59c3dc06d1b912710da3c051e
13002457275 <4448445@qq.com>
2021-01-30 00:10:01 +0800

    11

--- 深度学习/扩展pytorch/mnist 测试/myLR多分类实战.py
+++ 深度学习/扩展pytorch/mnist 测试/myLR多分类实战.py
@@ -37,49 +37,33 @@ torch.nn.init.kaiming_normal_(w2)
 torch.nn.init.kaiming_normal_(w3)
 
 
-# def f1(x,w,b):
-#     return x@w.t() + b
 class F1(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, x,w,b):
+    def forward(ctx, x, w,b):
         # x:[210,784],  w:[200,784], b[200,1]
-        ctx.save_for_backward(x,w,b)
-        print(f"开始正向传播")
-        X =  x@w.t() + b
+        ctx.save_for_backward(x, w,b)
+        # print(f"开始正向传播")
+        x = x @ w.t()+b
         # X torch.Size([210, 200])
-        return X,w,b
+        return x, w,b
 
     @staticmethod
     def backward(ctx, grad_x,grad_w,grad_b):
-        x,w,b = ctx.saved_tensors
+        O, w,b = ctx.saved_tensors
+        # O [210,784]  w[200,784]
         # error = np.dot(params['W3'].T, error) * self.sigmoid(params['X2'], derivative=True)
         # change_w['W2'] = np.outer(error, params['O1'])
-        # grad_w,grad_x,grad_b = grad_output
-        grad_x_new = grad_x * w.t()
-        # [210,200]
+        grad_x_new = grad_x @ w
+        # grad_x_new [210,784]
+        grad_w_new = grad_x.t() @ O
+        # grad_w_new [200,784]
+        # print(f"开始反向传播 grad_x is {grad_x.shape}")
+        return grad_x_new, grad_w_new,grad_b
 
-        grad_w_new = torch.mm(grad_x.T * x)
-
-        grad_b_new = grad_b
-
-        print(f"开始反向传播 grad_x is {grad_x}")
-        return grad_x_new,grad_w_new,grad_b_new
-
-# class F1Layer(nn.Module):
-#     def forward(self, x,w1,b1):
-#         x,w1, b1 = F1.apply(O0, w1, b1)
-#         return x
-
-# def forward(x):
-#     x = x@w1.t() + b1
-#     x = F.relu(x)
-#     x = x@w2.t() + b2
-#     x = F.relu(x)
-#     x = x@w3.t() + b3
-#     x = F.relu(x)
-#     return x
-
-
+class F1Layer(nn.Module):
+    def forward(self, x,w1,b1):
+        X1,w11,b11 = F1.apply(x,w1,b1)
+        return X1
 
 optimizer = optim.SGD([w1, b1, w2, b2, w3, b3], lr=learning_rate)
 criteon = nn.CrossEntropyLoss()
@@ -89,11 +73,11 @@ for epoch in range(epochs):
     for batch_idx, (data, target) in enumerate(train_loader):
         data = data.view(-1, 28*28)
         O0 = data
-        X1,w1,b1 = F1.apply(O0,w1,b1)
+        X1,w11,b11 = F1.apply(O0,w1,b1)
         O1 = F.relu(X1)
-        X2,w2, b2 = F1.apply(O1, w2, b2)
+        X2,w22, b22 = F1.apply(O1, w2, b2)
         O2 = F.relu(X2)
-        X3,w3, b3 = F1.apply(O2, w3, b3)
+        X3,w33, b33 = F1.apply(O2, w3, b3)
         O3 = F.relu(X3)
         logits = O3
         loss = criteon(logits, target)
@@ -115,11 +99,11 @@ for epoch in range(epochs):
         data = data.view(-1, 28 * 28)
         data = data.view(-1, 28 * 28)
         O0 = torch.tensor(data)
-        X1 = F1.apply(O0, w1, b1)
+        X1,w11,b11 = F1.apply(O0, w1, b1)
         O1 = F.relu(X1)
-        X2 = F1.apply(O1, w2, b2)
+        X2,w22,b22 = F1.apply(O1, w2, b2)
         O2 = F.relu(X2)
-        X3 = F1.apply(O2, w3, b3)
+        X3,w33,b33 = F1.apply(O2, w3, b3)
         O3 = F.relu(X3)
         logits = O3
         test_loss += criteon(logits, target).item()
@@ -131,3 +115,10 @@ for epoch in range(epochs):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+
+
+# Train Epoch: 9 [0/60000 (0%)]	Loss: 0.119061
+# Train Epoch: 9 [20000/60000 (33%)]	Loss: 0.130722
+# Train Epoch: 9 [40000/60000 (67%)]	Loss: 0.137054
+#
+# Test set: Average loss: 0.0008, Accuracy: 9548/10000 (95%)
