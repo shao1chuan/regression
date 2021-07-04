8c43898c2d92c638e8c202e70edef4ec6e515719
13002457275 <4448445@qq.com>
2021-07-04 17:47:34 +0800

    new

--- 深度学习与pytorch课程代码/12 迁移学习/12.99 Dataset step6 visdom.py
+++ 深度学习与pytorch课程代码/12 迁移学习/12.99 Dataset step6 visdom.py
@@ -1,16 +1,30 @@
-# 重要语法
-# 1 filenames = os.listdir(root)  取得目录下文件夹名
+
+# python -m visdom.server
 import torch
 from torch.utils.data  import Dataset, DataLoader
 import os,glob,csv,random
 from torchvision import transforms
 from PIL import Image
+import  visdom
+import  time
+
 
 from ImageDatastep5 import ImageData
 def main():
-    ds = ImageData(r"pokemon\data","224","train")
+    ds = ImageData(r"pokemon\data",64,"train")
     x,y = next(iter(ds))
     print(x.shape,y.shape)
+
+    viz = visdom.Visdom()
+    viz.image(ds.denormalize(x), win='sample_x', opts=dict(title='sample_x'))
+    loader = DataLoader(ds, batch_size=32, shuffle=True, num_workers=8)
+
+    for x,y in loader:
+        viz.images(ds.denormalize(x), nrow=8, win='batch', opts=dict(title='batch'))
+        viz.text(str(y.numpy()), win='label', opts=dict(title='batch-y'))
+
+        time.sleep(10)
+
 if __name__ == '__main__':
     main()
 
--- 深度学习与pytorch课程代码/12 迁移学习/ImageDatastep5.py
+++ 深度学习与pytorch课程代码/12 迁移学习/ImageDatastep5.py
@@ -23,7 +23,7 @@ class ImageData(Dataset):
                 continue
             self.name2label[name] = idx
         print(self.name2label)
-        self.images,self.labels = self.load_csv(root)
+        self.images,self.labels = self.load_csv('images.csv')
         if mode == 'train':
             self.images,self.labels = self.images[:int(0.6*len(self.images))],self.labels[:int(0.6*len(self.images))]
         elif mode =='val':
@@ -38,30 +38,49 @@ class ImageData(Dataset):
         # 'pokemon\\data\\squirtle\\00000205.jpg' 变成文件
         img,label  = self.images[idx],self.labels[idx]
         tf = transforms.Compose([
-            lambda x:Image.open(x).convert('RGB'),
-            transforms.Resize(self.resize,self.resize),
-            transforms.ToTensor
+            lambda x: Image.open(x).convert('RGB'),  # string path= > image data
+            transforms.Resize((int(self.resize * 1.25), int(self.resize * 1.25))),
+            transforms.RandomRotation(15),
+            transforms.CenterCrop(self.resize),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
         ])
         img = tf(img)
         label = torch.tensor(label)
         return img,label
+    def denormalize(self, x_hat):
 
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+
+        # x_hat = (x-mean)/std
+        # x = x_hat*std = mean
+        # x: [c, h, w]
+        # mean: [3] => [3, 4, 4]
+        mean = torch.tensor(mean).unsqueeze(1).unsqueeze(1)
+        std = torch.tensor(std).unsqueeze(1).unsqueeze(1)
+        # print(mean.shape, std.shape)
+        x = x_hat * std + mean
+
+        return x
     def load_csv(self,filename):
-        images = []
-        for name in self.name2label.keys():
-            images+= glob.glob(os.path.join(self.root,name,'*.png'))
-            images+= glob.glob(os.path.join(self.root,name,'*.jpg'))
-            images+= glob.glob(os.path.join(self.root,name,'*.jpeg'))
-        print(len(images),images)
-        random.shuffle(images)
         if not os.path.exists(os.path.join(self.root, filename)):
+            images = []
+            for name in self.name2label.keys():
+                images+= glob.glob(os.path.join(self.root,name,'*.png'))
+                images+= glob.glob(os.path.join(self.root,name,'*.jpg'))
+                images+= glob.glob(os.path.join(self.root,name,'*.jpeg'))
+            print(len(images),images)
+            random.shuffle(images)
+
             with open(os.path.join(self.root, filename),mode="w",newline='') as f:
-                writer = csv.writer(f)
-                for image in images:
-                    # 'pokemon\\data\\squirtle\\00000205.jpg'
-                    name = image.split(os.sep)[-2]
-                    label = self.name2label[name]
-                    writer.writerow([image,label])
+                    writer = csv.writer(f)
+                    for image in images:
+                        # 'pokemon\\data\\squirtle\\00000205.jpg'
+                        name = image.split(os.sep)[-2]
+                        label = self.name2label[name]
+                        writer.writerow([image,label])
                     print('writen into csv file:', filename)
         images, labels = [], []
         with open(os.path.join(self.root, filename)) as f:
