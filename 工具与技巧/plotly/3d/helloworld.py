4ec9e40b1eafb697f89e956d1c625d8bb8c10ada
13002457275 <4448445@qq.com>
2021-10-13 22:32:43 +0800

    new

--- 工具与技巧/plotly/3d/helloworld.py
+++ 工具与技巧/plotly/3d/helloworld.py
@@ -11,4 +11,5 @@ fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
 fig.update_layout(title='Mt Bruno Elevation', autosize=False,
                   width=500, height=500,
                   margin=dict(l=65, r=50, b=65, t=90))
+fig.update()
 fig.show()
